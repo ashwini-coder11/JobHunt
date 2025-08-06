e5fbf9b170ae8cb87b3a386b8f65d3092d1b0b1c
ashwini-coder11 <146967126+ashwini-coder11@users.noreply.github.com>
2025-08-06 16:29:10 +0530

    Update user.route.js

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,7 +4,7 @@ import isAuthenticated from "../middlewares/isAuthenticated.js";
 //import { singleUpload } from "../middlewares/mutler.js";
  
 const router = express.Router();
-
+//paths
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
